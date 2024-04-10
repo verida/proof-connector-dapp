3d706e1ec4e23de2c9df86f34882d63c33a0816c
icetrust0212 <zaza.liveedu@gmail.com>
2024-04-09 23:29:12 -0400

    update button css

--- components/ModalButton.tsx
+++ components/ModalButton.tsx
@@ -6,7 +6,7 @@ export const ModalButton: React.FC<Props> = ({ onClick, title }) => {
   return (
     <button
       onClick={() => onClick()}
-      className="bg-[#5354D1] text-white text-[16px] rounded-md w-full py-3 flex justify-center items-center"
+      className="bg-[#5354D1] hover:bg-[#6b6bd8] text-white text-[16px] rounded-md w-full py-3 flex justify-center items-center"
     >
       {title}
     </button>
