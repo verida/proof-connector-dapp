c2055e0ae68fc045038c22bc940197702167798b
icetrust0212 <zaza.liveedu@gmail.com>
2024-04-09 23:02:54 -0400

    update css

--- components/layouts/Footer.tsx
+++ components/layouts/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer: React.FC<{}> = () => {
     return (
-        <div className="flex px-24 py-5 text-[#ffffffaa] w-full justify-between text-[20px] mt-auto border-t-2 border-[#ffffffaa]">
+        <div className="flex px-5 py-3 md:px-24 md:py-5 text-[#ffffffaa] w-full justify-between text-[14px] md:text-[20px] mt-auto border-t-2 border-[#ffffffaa]">
             <h3 className="">© 2023 Verida Network</h3>
             <a href="https://www.verida.io/terms-and-conditions" target="_blank" className="footer-link-two no-margin">Terms &amp; Conditions</a>
         </div>
--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -6,10 +6,10 @@ interface Props {
 }
 export const Header: React.FC<Props> = ({handleClick, title, disabled}) => {
   return (
-    <div className="w-full flex items-center justify-between py-4 px-24 bg-[#ffffff22]">
+    <div className="w-full flex items-center justify-between px-4 py-4 md:px-24 bg-[#ffffff22]">
       <img src="/logo.svg" className="cursor-pointer"/>
       <button
-        className="border-1 px-5 py-3 cursor-pointer text-[18px] bg-white rounded-md text-[#19193d] hover:bg-[#eee]"
+        className="border-1 px-3 py-2 md:px-5 md:py-3 cursor-pointer text-[18px] bg-white rounded-md text-[#19193d] hover:bg-[#eee]"
         onClick={() => handleClick()}
         disabled={disabled}
       >
